4cc5cae9df2c373a56f69f90420c9ef05acaa735
shashankyerguntala <93209763+shashankyerguntala@users.noreply.github.com>
2025-07-14 09:43:31 +0530

    Update Task_3.dart

--- Assignment - 1/DartFundamentals/Task_3.dart
+++ Assignment - 1/DartFundamentals/Task_3.dart
@@ -1,3 +1,21 @@
+class Person{
+  int ?age;
+  String ? name;
+  greet(String name , int age){
+    print("Hello $name , Your $age  ");
+  }
+  void isAdult(int age){
+    if(age>=18){
+      print('true');
+    }
+    else{
+      print('false');
+    }
+  }
+
+}
 void main(){
-  
+     Person()
+   ..greet('shash',22)
+   ..isAdult(22);
 }
