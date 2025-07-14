90656ea75999df74f627bc6c84f5eab62b34afa9
shashankyerguntala <93209763+shashankyerguntala@users.noreply.github.com>
2025-07-14 10:00:22 +0530

    Add files via upload

---
+++ Cascade.dart
@@ -0,0 +1,12 @@
+class Person{
+  int ?age;
+  String ? name;
+  greet(String name , int age){
+    print("Hello $name , Your $age  ");
+  }
+}
+void main(){
+     Person()
+   ..greet('shash',22);
+   
+}
